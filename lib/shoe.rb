6d81cc466130d7e9daef3b5a6c1e90d9f1ecd71a
mghanstad <mghanstad@gmail.com>
2018-07-13 14:12:06 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,11 +1,12 @@
 # Make your shoe class here!
 class Shoe
   
-  attr_reader :brand, :style, :color
+  attr_reader :brand, :color
   attr_accessor :size
   
   def initialize(brand)
     @brand = brand
   end 
 end
-    
+
+nike = Shoe.new("Nike", "red")
