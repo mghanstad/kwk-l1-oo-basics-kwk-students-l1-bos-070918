6d4be9617379dc04e8708cba41b46939d362ea51
mghanstad <mghanstad@gmail.com>
2018-07-13 14:01:07 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -5,11 +5,8 @@ class Book
   attr_reader :title
   attr_accessor :author, :genre, :page_count
   
-  def initialize(title,author,genre,page_count)
+  def initialize(title)
     @title = title
-    @author = author
-    @genre = genre
-    @page_count = page_count
   end
   
 end
